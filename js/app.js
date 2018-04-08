da9027bab7b7d255beacec247213256968a541ea
Geoffrey Lorenz <geoffrey.lorenz@autodesk.com>
2018-04-08 21:52:05 +0200

    test: Comment on what to do next

--- js/app.js
+++ js/app.js
@@ -48,3 +48,36 @@ function startTimer () {
   }
 // no encoding for hours because most of the user will finish before an hour, I might add a rule to loose the game if over 10 mins
 }
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// create the Array of cards
+
+let nl = document.getElementsByClassName('card').querySelectorAll(.fas); //grab the icons in .card, will give a nodeList
+
+// transform the NodeList in an Array ??? Do not know if it works, I get undefined
+let Array = [...nl];
+
+
+// then I want to shuffle the (.fas each time I click on start button).
+
+// I want to click on the .card containing the .fas and a flip animation + a change of class to make the .fas visible happen --> I think I know how change the class in JS
+
+// then I want to compare the clickedCards
+
+// if they have the same class fas fa-bowling(for example), I want to apply a match class which will animate and color the background, the element must disappear, maybe display none
+
+// if they do not match, background becomes red, little animation and the cards turn again
