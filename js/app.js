00fa1a8ad3e5b62c4e67c725470eec4a5bf99632
Geoffrey Lorenz <geoffrey.lorenz@autodesk.com>
2018-04-11 18:05:36 +0200

    feat: Decreased number of stars depending on moves
    and some refactoring and comments edition

--- js/app.js
+++ js/app.js
@@ -3,23 +3,23 @@
 let minutes = 00,
     seconds = 00,
     addSeconds = document.getElementById("seconds"),
-    addMinutes = document.getElementById("minutes"), // all the above was linked to time
+    addMinutes = document.getElementById("minutes"), // All the above was linked to time
     Interval,
-    // link to click buttons
+    // Link to click buttons
     startButton = document.getElementById("start"),
     pauseButton = document.getElementById("pause"),
 
-    // get the elements from the are-of-game, cards, etc
+    // Get the elements from the are-of-game, cards, etc
     gameArea = document.getElementById("area-of-game"),
     card = document.getElementsByClassName("card"),
-    cards = [...card], // put the html collection in the Array
-    clickedCards = [], // store the value of clicked Cards, which will be always [0, 1]
+    cards = [...card], // Put the html collection in the Array
+    clickedCards = [], // Store the value of clicked Cards, which will be always [0, 1]
     drawingOfTheCard = document.getElementsByClassName(".card fas"),
     drawingOfTheCards = [...drawingOfTheCard],
-    //move related variables
+    // Move related variables
     moves = 0,
     movesCounter = document.getElementById("move"),
-    pairToFind = 8; // because 16 cards.
+    pairToFind = 8; // Because 16 cards.
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 
@@ -39,19 +39,19 @@ function shuffle(array) {
 // Initiate the game
 startButton.onclick = function startGame() {
   startButton.innerHTML = "Reset the Game"; // I was thinking about a reset button but I think this is more intuitive to just change start dynamically
-  // mix the deck by calling the shuffle function
+  // Mix the deck by calling the shuffle function
   shuffle(cards);
-  // remove the cards content for people who would have started the game without click on start. Indeed, the cards would have stayed unflipped / already counted
-  for (card of cards) { // looping over our 16 cards
-      gameArea.innerHTML = ""; // empty the board / area of the game
-      [].forEach.call(cards, function(inject) { // tweak from: Learn HTML5 and JavaScript for Android p.98
-        gameArea.appendChild(inject); // inject the array in the <li>
+  // Remove the cards content for people who would have started the game without click on start. Indeed, the cards would have stayed unflipped / already counted
+  for (card of cards) { // Looping over our 16 cards
+      gameArea.innerHTML = ""; // Empty the board / area of the game
+      [].forEach.call(cards, function(inject) { // Tweak from: Learn HTML5 and JavaScript for Android p.98
+        gameArea.appendChild(inject); // Inject the array in the <li>
     });
   card.classList.remove("matched", "not-matched", "flip");
   }
-  // start the timer
+  // Start the timer
   clearInterval(Interval); // https://www.w3schools.com/jsref/met_win_clearinterval.asp
-  Interval = setInterval(startTimer, 1000); // start the timer, 1000 = 1 second
+  Interval = setInterval(startTimer, 1000); // Start the timer, 1000 = 1 second
   seconds = "00";
   minutes = "00";
   addSeconds.innerHTML = seconds;
@@ -62,28 +62,28 @@ startButton.onclick = function startGame() {
   movesCounter.innerHTML = moves;
 }
 
-// click on a card
+// Click on a card
 for (card of cards) {
-  card.onclick = function cardsWaitingToBeCompared() {  // when a card is clicked on, it calls the function cardsWaitingToBeCompared
-  this.classList.toggle("flip");// use of "this." otherwise, when I click on a card, another one get flipped.
-  clickedCards.push(this); // which store the clicked card in the clickedCards array
+  card.onclick = function cardsWaitingToBeCompared() {  // When a card is clicked on, it calls the function cardsWaitingToBeCompared
+  this.classList.toggle("flip");// Use of "this." otherwise, when I click on a card, another one get flipped.
+  clickedCards.push(this); // Which store the clicked card in the clickedCards array
 
     if (clickedCards.length === 2 && clickedCards[0].innerHTML === clickedCards[1].innerHTML) { // Once two cards are clicked trigger the below actions
       match();
-    } else { // remove the flip
+    } else { // Remove the flip
       notMatched();
     }
     countAMove();
     clickedCards[0].classList.remove("flip", "not-matched");
     clickedCards[1].classList.remove("flip", "not-matched");
-    clickedCards = [];    // important as otherwise the clickedCards.length does not reset and go over 2 and countAMove & notMatched() are not triggered
+    clickedCards = [];    // Important as otherwise the clickedCards.length does not reset and go over 2 and countAMove & notMatched() are not triggered
     }
   }
 
-  // pause button and what it does
+  // Pause button and what it does
 pauseButton.onclick = function() {
-  clearInterval(Interval); // pause the timer
-  alert("You can come back later, the time has stopped!"); // when window is left, timer start again
+  clearInterval(Interval); // Pause the timer
+  alert("You can come back later, the time has stopped!"); // When window is left, timer start again
   Interval = setInterval(startTimer, 1000)
 }
 
@@ -91,17 +91,17 @@ function startTimer () {
   seconds++;
 
   if(seconds < 10){
-    addSeconds.innerHTML = "0" + seconds; // if seconds is inferior to 10 add a 0 in front --> so
+    addSeconds.innerHTML = "0" + seconds; // If seconds is inferior to 10 add a 0 in front --> so
   }
 
   if (seconds > 9){
-    addSeconds.innerHTML = seconds; // remove the 0 as it will be replaced by the tenths
+    addSeconds.innerHTML = seconds; // Remove the 0 as it will be replaced by the tenths
   }
 
-  if (seconds > 59) { // once 60 seconds is reached, a minute is added
+  if (seconds > 59) { // Once 60 seconds is reached, a minute is added
     minutes++;
-    addMinutes.innerHTML = "0" + minutes; // preceded by a 0, otherwise it is ugly
-    seconds = 0; // seconds is reset
+    addMinutes.innerHTML = "0" + minutes; // Preceded by a 0, otherwise it is ugly
+    seconds = 0; // Seconds is reset
     addSeconds.innerHTML = "0" + seconds;
   }
 
@@ -109,12 +109,24 @@ function startTimer () {
     addMinutes.innerHTML = minutes;
     loseGame();
   }
-// no encoding for hours because most of the user will finish before an hour, I might add a rule to loose the game if over 10 mins
+// So encoding for hours because most of the user will finish before an hour, I might add a rule to loose the game if over 10 mins
 }
 
 function countAMove() {
   moves++;
   movesCounter.innerHTML = moves;
+  if (moves > 12) {
+    star--;
+  }
+  else if (move > 15) {
+    star--;
+  }
+  else if (move > 20) {
+    star--;
+  }
+  else {
+    loseGame();
+  }
 }
 
 function loseGame()
@@ -130,7 +142,7 @@ function winGame() {
 }
 
 function match() {
-  clickedCards[0].classList.toggle("matched"); // to trigger matched. otherwise, it's half done animation.
+  clickedCards[0].classList.toggle("matched"); // To trigger matched. otherwise, it's half done animation.
   clickedCards[1].classList.toggle("matched");
   pairToFind--;
   winGame();
