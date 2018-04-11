52a2d2716162a8766a26e943e8ca0ebcf245668f
Geoffrey Lorenz <geoffrey.lorenz@autodesk.com>
2018-04-11 15:22:18 +0200

    git commit -msstyle: Changed a few things/removed blanks/lines

--- js/app.js
+++ js/app.js
@@ -19,68 +19,64 @@ let minutes = 00,
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 
-        function shuffle(array) {
-        let currentIndex = array.length, temporaryValue, randomIndex;
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
 
-        while (currentIndex !== 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-
-        return array;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
 
 // Initiate the game
 
 startButton.onclick = function startGame() {
 
-// mix the deck by calling the shuffle function
-shuffle(cards);
-clearInterval(Interval); // https://www.w3schools.com/jsref/met_win_clearinterval.asp
-pairToFind = 8;
-Interval = setInterval(startTimer, 1000); // start the timer, 1000 = 1 second
-seconds = "00";
-minutes = "00";
-addSeconds.innerHTML = seconds;
-addMinutes.innerHTML = minutes;
-startButton.innerHTML = "Reset the Game"; // I was thinking about a reset button but I think this is more intuitive to just change start dynamically
-for (card of cards) { // looping over our 16 cards
-  gameArea.innerHTML = ""; // empty the board / area of the game
-  [].forEach.call(cards, function(inject) { // tweak from: Learn HTML5 and JavaScript for Android p.98
-            gameArea.appendChild(inject); // inject the array in the <li>
-
-        });
+  // mix the deck by calling the shuffle function
+  shuffle(cards);
+  clearInterval(Interval); // https://www.w3schools.com/jsref/met_win_clearinterval.asp
+  pairToFind = 8;
+  Interval = setInterval(startTimer, 1000); // start the timer, 1000 = 1 second
+  seconds = "00";
+  minutes = "00";
+  addSeconds.innerHTML = seconds;
+  addMinutes.innerHTML = minutes;
+  startButton.innerHTML = "Reset the Game"; // I was thinking about a reset button but I think this is more intuitive to just change start dynamically
+    for (card of cards) { // looping over our 16 cards
+      gameArea.innerHTML = ""; // empty the board / area of the game
+      [].forEach.call(cards, function(inject) { // tweak from: Learn HTML5 and JavaScript for Android p.98
+        gameArea.appendChild(inject); // inject the array in the <li>
+    });
   }
 }
 
 for (card of cards) {
   card.onclick = function cardsWaitingToBeCompared() {  // when a card is clicked on, it calls the function cardsWaitingToBeCompared
-  this.classList.add("flip");// use of "this." otherwise, when I click on a card, another one get flipped.
-  console.log(this);
-  clickedCards.push(this);
+  this.classList.toggle("flip");// use of "this." otherwise, when I click on a card, another one get flipped.
+  clickedCards.push(this); // which store the clicked card in the clickedCards array
 
- // which store the clicked card in the clickedCards array
     if (clickedCards.length === 2) { // Once two cards are clicked trigger the below actions
       countAMove();
-        if (clickedCards[0].innerHTML === clickedCards[1].innerHTML) {// I added .innerHTML and it solved my issue
-          match();
-        } else {
-            notMatched();
-          }
-        clickedCards = []; // important as otherwise the clickedCards.length does not reset and go over 2 and countAMove & notMatched() are not triggered
+
+      if (clickedCards[0].innerHTML === clickedCards[1].innerHTML) {// I added .innerHTML and it solved my issue
+        match();
+      } else { // remove the flip
+        notMatched();
         }
+    clickedCards = [];    // important as otherwise the clickedCards.length does not reset and go over 2 and countAMove & notMatched() are not triggered
+    }
   }
 }
 
   // pause button and what it does
 pauseButton.onclick = function() {
-
-clearInterval(Interval); // pause the timer
-alert("You can come back later, the time has stopped!"); // when window is left, timer start again
-Interval = setInterval(startTimer, 1000)
+  clearInterval(Interval); // pause the timer
+  alert("You can come back later, the time has stopped!"); // when window is left, timer start again
+  Interval = setInterval(startTimer, 1000)
 }
 
 function startTimer () {
@@ -114,20 +110,19 @@ function countAMove() {
 
 function endGame() {
   if (pairToFind === 0) {
-  console.log("The game ends open a Windows to show the stats and ask if he wants to reset");
+    console.log("The game ends open a Windows to show the stats and ask if he wants to reset");
   }
+  return false;
 }
 
 function match() {
-  clickedCards[0].classList.add("matched");
-  clickedCards[1].classList.add("matched");
+  clickedCards[0].classList.toggle("matched"); // to trigger matched. otherwise, it's half done animation.
+  clickedCards[1].classList.toggle("matched");
   pairToFind--;
   endGame();
 }
 
-
-
 function notMatched() {
-  clickedCards[0].classList.add("not-matched");
-  clickedCards[1].classList.add("not-matched");
+  clickedCards[0].classList.toggle("not-matched");
+  clickedCards[1].classList.toggle("not-matched");
 }
