57cd796c4c04d936b67b1cedb4e043dc5d233cd1
Geoffrey Lorenz <geoffrey.lorenz@autodesk.com>
2018-04-11 21:51:42 +0200

    feat: Removes the stars based on moves
    it works in the back if I call stars; but the innerHTML is messed up.
    The restart button does not provide the relevant HTML

--- js/app.js
+++ js/app.js
@@ -31,23 +31,19 @@ for (card of cards) {
   this.classList.toggle("flip");// Use of "this." otherwise, when I click on a card, another one get flipped.
   clickedCards.push(this); // Which store the clicked card in the clickedCards array
 
-
     if (clickedCards.length === 2 && clickedCards[0].innerHTML === clickedCards[1].innerHTML) { // Once two cards are clicked trigger the below actions
       match();
       countAMove();
+      clickedCards = [];
     }
-    else if(clickedCards.length === 2 && clickedCards[0].innerHTML !== clickedCards[1].innerHTML) {
-        clickedCards[0].classList.toggle("not-matched");
-        console.log(clickedCards[1]);
-        clickedCards[1].classList.toggle("not-matched");
-        console.log(clickedCards[0]);
-        clickedCards[0].classList.remove("not-matched");
-        clickedCards[1].classList.remove("not-matched");
-        clickedCards = [];    // Important as otherwise the clickedCards.length does not reset and go over 2 and countAMove & notMatched() are not triggered
-      }
-      }
 
+    else if (clickedCards.length === 2 && clickedCards[0].innerHTML !== clickedCards[1].innerHTML) {
+        unMatched();
+        countAMove();   // Important as otherwise the clickedCards.length does not reset and go over 2 and countAMove & notMatched() are not triggered
+        clickedCards = [];
+    }
   }
+}
 
   // Pause button and what it does
 pauseButton.onclick = function() {
@@ -56,7 +52,7 @@ pauseButton.onclick = function() {
   Interval = setInterval(startTimer, 1000)
 }
 
-function startTimer () {
+function startTimer() {
   seconds++;
 
   if(seconds < 10){
@@ -75,31 +71,35 @@ function startTimer () {
     addMinutes.innerHTML = minutes;
     loseGame();
   }
-// So encoding for hours because most of the user will finish before an hour, I might add a rule to loose the game if over 10 mins
+// No encoding for hours because most of the user will finish before an hour, I might add a rule to loose the game if over 10 mins
 }
 //
-for (star of stars) { // Looping over our 3 stars
-    starsContainer.innerHTML = ""; // Empty the container;
-    [].forEach.call(stars, function(inject) { // Tweak from: Learn HTML5 and JavaScript for Android p.98
-      starsContainer.appendChild(inject); // Inject the array in the <li>
-  });
-
-card.classList.remove("matched", "not-matched", "flip");
-}
-
 function countAMove() {
   moves++;
   movesCounter.innerHTML = moves;
-  if (moves > 12) {
-    star--;
+  if (moves === 1) {
+    stars.shift();
+    starsContainer.innerHTML = starsContainer.innerHTML;
+    console.log(stars);
   }
-  else if (moves > 15) {
-    star--;
+  else if (moves === 3) {
+    stars.shift();
+    starsContainer.innerHTML = "";
+    console.log(stars);
   }
-  else if (moves > 20) {
-    star--;
+
+  else if (moves === 5) {
+    stars.shift();
+    starsContainer.innerHTML = stars.length;
+    console.log(stars);
+
   }
-}
+  else if (moves > 8)
+    {
+      console.log("functionEndGame")
+    }
+  }
+
 
 function match() {
   clickedCards[0].classList.toggle("matched"); // To trigger matched. otherwise, it's half done animation.
@@ -107,11 +107,9 @@ function match() {
   pairToFind--;
 }
 
-function loseGame()
-{
-  if (moves > 25 || minutes > 10) {
-    alert("you lost");
-  }
+function unMatched() {
+  clickedCards[0].classList.toggle("not-matched");
+  clickedCards[1].classList.toggle("not-matched");
 }
 
 function winGame() {
@@ -135,6 +133,14 @@ startButton.onclick = function startGame() {
     });
   card.classList.remove("matched", "not-matched", "flip");
   }
+  // Reset the stars.
+  for (star of stars) { // Looping over our 3 stars
+      starsContainer.innerHTML = ""; // Empty the container;
+      [].forEach.call(stars, function(inject) { // Tweak from: Learn HTML5 and JavaScript for Android p.98
+        starsContainer.appendChild(inject); // Inject the array in the <li>
+    });
+}
+
   // Start the timer
   clearInterval(Interval); // https://www.w3schools.com/jsref/met_win_clearinterval.asp
   Interval = setInterval(startTimer, 1000); // Start the timer, 1000 = 1 second
